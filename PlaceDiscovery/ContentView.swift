96ced8a014428789d0cf190baf4b26336d788a2d
vkuday <volkuday@hotmail.com>
2021-05-23 17:12:33 -0700

    Implemented views on Discovery page. Created category struct

--- PlaceDiscovery/ContentView.swift
+++ PlaceDiscovery/ContentView.swift
@@ -15,6 +15,11 @@ struct ContentView: View {
                 
                 DiscoverCategoriesView()
                 
+                PopularDestinationView()
+                
+                PopularRestaurantsView()
+                
+                TrendingCreatorsView()
                 
             }.navigationTitle("Discover")
         }
@@ -27,20 +32,120 @@ struct ContentView_Previews: PreviewProvider {
     }
 }
 
-struct DiscoverCategoriesView: View {
+struct PopularDestinationView: View {
     var body: some View {
-        ScrollView(.horizontal, showsIndicators: false) {
-            HStack(spacing: 8) {
-                ForEach(0..<5, id: \.self) { num in
-                    VStack(spacing: 4) {
+        VStack {
+            HStack {
+                Text("Popular Destination")
+                    .font(.system(size: 14, weight: .semibold))
+                Spacer()
+                Text("See all")
+                    .font(.system(size: 12, weight: .semibold))
+            }.padding(.horizontal)
+            .padding(.top)
+            
+            ScrollView(.horizontal) {
+                HStack(spacing: 8) {
+                    ForEach(0..<5, id: \.self) { num in
+                        Spacer()
+                            .frame(width: 125, height: 150)
+                            .background(Color.gray)
+                            .cornerRadius(5)
+                            .shadow(color: .gray, radius: 4, x: 0.0, y: 2)
+                            .padding(.bottom)
+                    }
+                }.padding(.horizontal)
+            }
+        }
+    }
+}
+
+struct PopularRestaurantsView: View {
+    var body: some View {
+        VStack {
+            HStack {
+                Text("Popular places to eat")
+                    .font(.system(size: 14, weight: .semibold))
+                Spacer()
+                Text("See all")
+                    .font(.system(size: 12, weight: .semibold))
+            }.padding(.horizontal)
+            .padding(.top)
+            
+            ScrollView(.horizontal) {
+                HStack(spacing: 8) {
+                    ForEach(0..<5, id: \.self) { num in
+                        Spacer()
+                            .frame(width: 200, height: 64)
+                            .background(Color.gray)
+                            .cornerRadius(5)
+                            .shadow(color: .gray, radius: 4, x: 0.0, y: 2)
+                            .padding(.bottom)
+                    }
+                }.padding(.horizontal)
+            }
+        }
+    }
+}
+
+struct TrendingCreatorsView: View {
+    var body: some View {
+        VStack {
+            HStack {
+                Text("Trending Creators")
+                    .font(.system(size: 14, weight: .semibold))
+                Spacer()
+                Text("See all")
+                    .font(.system(size: 12, weight: .semibold))
+            }.padding(.horizontal)
+            .padding(.top)
+            
+            ScrollView(.horizontal) {
+                HStack(spacing: 8) {
+                    ForEach(0..<15, id: \.self) { num in
                         Spacer()
                             .frame(width: 50, height: 50)
                             .background(Color.gray)
                             .cornerRadius(.infinity)
                             .shadow(color: .gray, radius: 4, x: 0.0, y: 2)
-                        Text("TEST")
-                            .font(.system(size: 12, weight: .semibold))
+                            .padding(.bottom)
                     }
+                }.padding(.horizontal)
+            }
+        }
+    }
+}
+
+struct Category: Hashable {
+    let name, imageName: String
+}
+
+struct DiscoverCategoriesView: View {
+    
+    let categories: [Category] = [
+        .init(name: "Art", imageName: "paintpalette.fill"),
+        .init(name: "Sport", imageName: "sportscourt.fill"),
+        .init(name: "Live Events", imageName: "music.mic"),
+        .init(name: "Food", imageName: "palette"),
+        .init(name: "History", imageName: "palette"),
+    ]
+    
+    var body: some View {
+        ScrollView(.horizontal, showsIndicators: false) {
+            HStack(alignment: .top, spacing: 14) {
+                ForEach(categories, id: \.self) { category in
+                    VStack(spacing: 8) {
+                        Image(systemName: category.imageName)
+                            .font(.system(size: 20))
+                            .foregroundColor(.white)
+                            .frame(width: 64, height: 64)
+                            .background(Color.gray)
+                            .cornerRadius(64)
+                            .shadow(color: .gray, radius: 4, x: 0.0, y: 2)
+                        Text(category.name)
+                            .font(.system(size: 12, weight: .semibold))
+                            .multilineTextAlignment(.center)
+                    }.frame(width: 68)
                 }
             }.padding(.horizontal)
         }
